ca1f74c0bce9cf5b76b07b5fb8afbc95a8616a7b
TedMdelaCruz <tedmdelacruz@gmail.com>
2013-04-20 13:25:25 +0800

    Initial commit

---
+++ js/application.js
@@ -0,0 +1,54 @@
+$(function(){
+  function filterPath(string) {
+    return string
+      .replace(/^\//,'')
+      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+      .replace(/\/$/,'');
+  }
+
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i < argLength; i++) {
+      var el = arguments[i];
+      var $scrollElement = $(el);
+
+      if ($scrollElement.scrollTop() > 0) {
+
+        return el;
+
+      } else {
+
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop() > 0;
+        $scrollElement.scrollTop(0);
+
+        if (isScrollable) {
+          return el;
+        }
+      } // else
+    } // for
+    return [];
+  } // function
+
+  // Get all anchor elements with # targets
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if ( locationPath == thisPath &&
+       ( location.hostname == this.hostname || !this.hostname) &&
+         this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  }); //$('a[href*=#]').each()
+}); // $(function)
